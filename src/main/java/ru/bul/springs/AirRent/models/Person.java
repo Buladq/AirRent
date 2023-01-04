68b7c8ae60643893eef284b966230d1798e7d861
Buladq <chumakov.bulat@yandex.ru>
2023-01-04 22:48:47 +0300

    создал основную модель модель

---
+++ src/main/java/ru/bul/springs/AirRent/models/Person.java
@@ -0,0 +1,109 @@
+package ru.bul.springs.AirRent.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty
+    @Size(min = 2, max = 60, message = "Имя должно быть от 2 до 60 символов")
+    @Column(name = "username")
+    private String username;
+
+
+    @Column(name = "password")
+    @Size(min = 8,max = 60,message = "Пароль должен состоять от 8 до 60 символов")
+    private String password;
+
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "email")
+    @NotEmpty(message = "Поле почты не должно быть пустым")
+    @Email(message = "Неверный формат почты")
+    private String email;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+
+
+
+    @Column(name = "activite")
+    private boolean activite;
+
+    public Person() {
+    }
+
+    public Person(String username, String role, String email, String phoneNumber) {
+        this.username = username;
+        this.role = role;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isActivite() {
+        return activite;
+    }
+
+    public void setActivite(boolean activite) {
+        this.activite = activite;
+    }
+}
