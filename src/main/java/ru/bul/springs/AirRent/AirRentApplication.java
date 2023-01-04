f382bf9f5a3b4c1c3c2c5f7da9e04b1763e82033
Buladq <chumakov.bulat@yandex.ru>
2023-01-04 22:45:10 +0300

    Собрал проект

---
+++ src/main/java/ru/bul/springs/AirRent/AirRentApplication.java
@@ -0,0 +1,13 @@
+package ru.bul.springs.AirRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bul/springs/AirRent/AirRentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bul.springs.AirRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
