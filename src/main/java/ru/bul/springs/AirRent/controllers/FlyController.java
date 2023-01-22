699189af39fc16a4cbdff0b0da1d7a71fc6769a8
Buladq <chumakov.bulat@yandex.ru>
2023-01-22 03:44:08 +0300

    Добавил страницу аренды самолета

--- src/main/java/ru/bul/springs/AirRent/controllers/FlyController.java
+++ src/main/java/ru/bul/springs/AirRent/controllers/FlyController.java
@@ -2,9 +2,7 @@ package ru.bul.springs.AirRent.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.bul.springs.AirRent.services.CityService;
 
 import java.time.LocalDate;
@@ -37,30 +35,68 @@ public class FlyController {
         model.addAttribute("nowMin",nowDate);
         model.addAttribute("cities",cityService.allCities());
 
-        if(!from.isEmpty()||!to.isEmpty()||!date.isEmpty()){
+        if(!from.isEmpty()&&!to.isEmpty()&&!date.isEmpty()&&!from.equals(to)){
             model.addAttribute("fromForInput",cityService.getNameById(Integer.parseInt(from)));
             model.addAttribute("toForInput",cityService.getNameById(Integer.parseInt(to)));
             model.addAttribute("dateForInput",date);
-
             model.addAttribute("fromValueForInput",from);
             model.addAttribute("toValueForInput",to);
+            model.addAttribute("good","good");
         }
 
-        if(from.isEmpty()||to.isEmpty()||date.isEmpty()){
-            System.out.println("пусто");
+        if(to.length()==0||date.length()==0||from.length()==0){
             model.addAttribute("theresEmpty","theresEmpty");
             return "fly/flight";
 
         }
         if(from.equals(to)){
-            System.out.println("Одинаковые значения");
             model.addAttribute("ones","ones");
+
             return "fly/flight";
 
         }
-        System.out.println(from);
-        System.out.println(to);
-        System.out.println(date);
+
         return "fly/flight";
+    }
+
+
+    @GetMapping("/rent")
+    public String rentAir(Model model, @RequestParam(value = "from",required = false)String from,
+                          @RequestParam(value = "to",required = false)String to,
+                          @RequestParam(value = "date",required = false)String date,
+                          @RequestParam(value = "time",required = false)String time){
+        model.addAttribute("cities",cityService.allCities());
+        return "fly/rentair";
+    }
+
+
+    @PostMapping("/rent")
+    public String rentAirPage(Model model, @RequestParam(value = "from",required = false)String from,
+                              @RequestParam(value = "to",required = false)String to,
+                              @RequestParam(value = "date",required = false)String date,
+                              @RequestParam(value = "time",required = false)String time){
+        LocalDate localDate=LocalDate.now();
+        String nowDate=localDate.toString();
+        model.addAttribute("nowMin",nowDate);
+        model.addAttribute("cities",cityService.allCities());
+
+        if(!from.isEmpty()&&!to.isEmpty()&&!date.isEmpty()&&!from.equals(to)&&!time.isEmpty()){
+            System.out.println("оплата!");
+        }
+
+        if(to.length()==0||date.length()==0||from.length()==0||time.length()==0){
+            model.addAttribute("theresEmpty","theresEmpty");
+            return "fly/rentair";
+
+        }
+
+        if(from.equals(to)){
+            model.addAttribute("ones","ones");
+
+            return "fly/rentair";
+
+        }
+
+        return "fly/rentair";
     }
 }
