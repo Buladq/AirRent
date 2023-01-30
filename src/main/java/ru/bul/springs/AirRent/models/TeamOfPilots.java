088e1c8861bd9b6b1cb2bbb21852b2b23771a1d3
Buladq <chumakov.bulat@yandex.ru>
2023-01-30 03:07:51 +0300

    Поменял генерацию

--- src/main/java/ru/bul/springs/AirRent/models/TeamOfPilots.java
+++ src/main/java/ru/bul/springs/AirRent/models/TeamOfPilots.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class TeamOfPilots {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @OneToOne
