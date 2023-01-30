38913a8730642483353c032989688582abfc20b6
Buladq <chumakov.bulat@yandex.ru>
2023-01-30 03:03:31 +0300

    метод для выччисление времени полета

--- src/main/java/ru/bul/springs/AirRent/Mak.java
+++ src/main/java/ru/bul/springs/AirRent/Mak.java
@@ -4,33 +4,47 @@ import java.util.Scanner;
 
 public class Mak {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter the latitude of the first city: ");
+//        double lat1 = scanner.nextDouble();
+//        System.out.print("Enter the longitude of the first city: ");
+//        double lon1 = scanner.nextDouble();
+//
+//        System.out.print("Enter the latitude of the second city: ");
+//        double lat2 = scanner.nextDouble();
+//        System.out.print("Enter the longitude of the second city: ");
+//        double lon2 = scanner.nextDouble();
+//
+//        double distance = distance(lat1, lon1, lat2, lon2);
+//        System.out.println("Distance between the cities: " + distance + " km");
+//    }
+//
+//    private static double distance(double lat1, double lon1, double lat2, double lon2) {
+//        double earthRadius = 6371; // km
+//        double dLat = Math.toRadians(lat2 - lat1);
+//        double dLon = Math.toRadians(lon2 - lon1);
+//        lat1 = Math.toRadians(lat1);
+//        lat2 = Math.toRadians(lat2);
+//
+//        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//                Math.cos(lat1) * Math.cos(lat2) *
+//                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+//        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//        return earthRadius * c;
+//    }
 
-        System.out.print("Enter the latitude of the first city: ");
-        double lat1 = scanner.nextDouble();
-        System.out.print("Enter the longitude of the first city: ");
-        double lon1 = scanner.nextDouble();
 
-        System.out.print("Enter the latitude of the second city: ");
-        double lat2 = scanner.nextDouble();
-        System.out.print("Enter the longitude of the second city: ");
-        double lon2 = scanner.nextDouble();
+//        ВРЕМЯ ПОЛЕТА!!!
 
-        double distance = distance(lat1, lon1, lat2, lon2);
-        System.out.println("Distance between the cities: " + distance + " km");
-    }
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter the flight distance (in km): ");
+//        double distance = sc.nextDouble();
+//        System.out.print("Enter the average speed (in km/h): ");
+//        double speed = sc.nextDouble();
+//        double time = distance / speed;
+//        int timeInMinutes = (int) (time * 60);
+//        System.out.println("The flight time is: " + timeInMinutes + " minutes");
 
-    private static double distance(double lat1, double lon1, double lat2, double lon2) {
-        double earthRadius = 6371; // km
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lon2 - lon1);
-        lat1 = Math.toRadians(lat1);
-        lat2 = Math.toRadians(lat2);
-
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.cos(lat1) * Math.cos(lat2) *
-                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return earthRadius * c;
     }
 }
